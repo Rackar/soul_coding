25a6de861b51223a8b8d458f990392686aa5ec3b
Your Name <27681135@qq.com>
2019-05-27 17:46:19 +0800

    一点首页

---
+++ src/views/soft.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="soft">
+    <div class="left">
+      <img :src="soft.img_url" alt />
+    </div>
+    <div class="right">
+      <el-row type="flex" class="row-bg" justify="space-around">
+        <el-col :span="8">
+          <span class="title"> {{ soft.name }}</span>
+        </el-col>
+        <el-col :span="8">{{ soft.type }}</el-col>
+        <el-col :span="8">
+          <el-link :href="soft.link" target="_blank">链接地址</el-link>
+        </el-col>
+      </el-row>
+      <div class="content">{{ soft.content }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    soft: {
+      type: Object,
+      default() {
+        return {
+          name: "软件名称",
+          type: "网页",
+          link: "http://sdfjlsdf",
+          content:
+            "软件详细介绍。软件详细介绍。软件详细介绍。软件详细介绍。软件详细介绍。软件详细介绍。软件详细介绍。软件详细介绍。软件详细介绍。软件详细介绍。软件详细介绍。软件详细介绍。软件详细介绍。软件详细介绍。软件详细介绍。软件详细介绍。软件详细介绍。软件详细介绍。软件详细介绍。软件详细介绍。软件详细介绍。软件详细介绍。软件详细介绍。软件详细介绍。",
+          img_url: "/img/calc.png"
+        };
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.soft {
+  border-bottom: 1px solid rgb(185, 184, 184);
+  width: 100%;
+  margin: 10px 5px;
+
+  text-align: center;
+  color: #303133;
+  //   width: 100%;
+  .left {
+    margin: 1%;
+    display: inline-block;
+    width: 150px;
+    img {
+      width: 150px;
+
+      // float: left;
+    }
+  }
+  .right {
+    display: inline-block;
+    vertical-align: top;
+    text-align: left;
+    width: 72%;
+    margin: 1%;
+    .el-row {
+      .el-col {
+        display: inline-block;
+        border-top: 1px;
+        border-bottom: 1px;
+        font-size: 20px;
+        .title {
+          font-family: Arial, Helvetica, sans-serif;
+          background-color: rgb(214, 255, 255);
+          border-radius: 5px;
+          padding: 1px 4px;
+          font-weight: bold;
+        }
+      }
+      .content {
+        //   border:1px solid gray;
+        display: inline-block;
+      }
+    }
+  }
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
+const productionGzipExtensions = ["js", "css"];
+const isProduction = process.env.NODE_ENV === "production";
+
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
 
@@ -5,10 +9,22 @@ module.exports = {
   devServer: {
     // publicPath: "/", // 和 publicPath 保持一致,
     port: 9527
-  }
+  },
   // configureWebpack: config => {
   //   if (process.env.NODE_ENV === "production") {
   //     config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true;
   //   }
   // }
+  configureWebpack: config => {
+    if (isProduction) {
+      config.plugins.push(
+        new CompressionWebpackPlugin({
+          algorithm: "gzip",
+          test: new RegExp("\\.(" + productionGzipExtensions.join("|") + ")$"),
+          threshold: 10240,
+          minRatio: 0.8
+        })
+      );
+    }
+  }
 };
